d2a5143dfc48f3e0efd8152f0b7723aaf34b9f0b
Cristian <63931867+crissancar@users.noreply.github.com>
2024-06-06 18:59:52 +0200

    chore: test failed (#7)
    * chore: test failed
    
    * ci: add test workflow
    
    * ci: update steps
    
    * ci: update

--- src/modules/status/controllers/StatusGetController.ts
+++ src/modules/status/controllers/StatusGetController.ts
@@ -4,6 +4,7 @@ import httpStatus from 'http-status';
 export class StatusGetController {
 	run(req: Request, res: Response): void {
 		console.log(req.headers);
+		throw new Error('Fake error');
 		res.status(httpStatus.OK).json({ message: 'Hello Azure!', headers: req.headers });
 	}
 }
