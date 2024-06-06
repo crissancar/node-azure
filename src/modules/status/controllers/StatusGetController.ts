965b3c036d0f6719fa6649bbb695fab9dd74ba5f
Cristian Sánchez <devcristiansanchez@gmail.com>
2024-06-06 20:07:03 +0200

    chore: remove fake error

--- src/modules/status/controllers/StatusGetController.ts
+++ src/modules/status/controllers/StatusGetController.ts
@@ -4,7 +4,6 @@ import httpStatus from 'http-status';
 export class StatusGetController {
 	run(req: Request, res: Response): void {
 		console.log(req.headers);
-		throw new Error('Fake error');
 
 		res.status(httpStatus.OK).json({ message: 'Hello Azure!' });
 	}
