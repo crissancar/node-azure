024fce066ce33a45c6ab4fa8b71c94430ab53963
Cristian Sánchez <devcristiansanchez@gmail.com>
2024-06-06 18:16:54 +0200

    ci: trial

--- src/modules/status/controllers/StatusGetController.ts
+++ src/modules/status/controllers/StatusGetController.ts
@@ -4,6 +4,6 @@ import httpStatus from 'http-status';
 export class StatusGetController {
 	run(req: Request, res: Response): void {
 		console.log(req.headers);
-		res.status(httpStatus.OK).send();
+		res.status(httpStatus.OK).json({ message: 'Hello Azure!', headers: req.headers });
 	}
 }
