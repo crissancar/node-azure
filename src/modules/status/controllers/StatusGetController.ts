2394d505ce4cd730b48053abde147a4da9c76f13
Cristian <63931867+crissancar@users.noreply.github.com>
2024-06-16 00:35:22 +0200

    chore(status-get-controller): remove log

--- src/modules/status/controllers/StatusGetController.ts
+++ src/modules/status/controllers/StatusGetController.ts
@@ -2,9 +2,7 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
 export class StatusGetController {
-	run(req: Request, res: Response): void {
-		console.log(req.headers);
-
+	run(req: Request, res: Response): void
 		res.status(httpStatus.OK).json({ message: 'Hello Azure!', version: '' });
 	}
 }
