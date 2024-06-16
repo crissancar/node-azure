10049d883fc4a8b53e14508b9334b4502b64b048
Cristian Sánchez <devcristiansanchez@gmail.com>
2024-06-16 10:03:00 +0200

    feat(status-get-controller): update response

--- src/modules/status/controllers/StatusGetController.ts
+++ src/modules/status/controllers/StatusGetController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
+import { version } from '../../../../package.json';
+
 export class StatusGetController {
 	run(req: Request, res: Response): void {
-		res.status(httpStatus.OK).json({ message: 'Hello Azure!', version: '' });
+		res.status(httpStatus.OK).json({ message: 'Hello Azure!', packageVersion: version });
 	}
 }
